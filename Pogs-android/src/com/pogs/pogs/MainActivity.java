640c6c42833220f92973a5e26947f98118044477
pogsdbest <arnel.espiritu@gmail.com>
2013-09-21 07:15:27 +0800

    first commit

---
+++ Pogs-android/src/com/pogs/pogs/MainActivity.java
@@ -0,0 +1,18 @@
+package com.pogs.pogs;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class MainActivity extends AndroidApplication {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        cfg.useGL20 = false;
+        
+        initialize(new PogsMain(), cfg);
+    }
+}
